06999af0343b8ead514297f10397e91b6d4ee967
Feldrise <admin@feldrise.com>
2021-08-23 12:27:48 +0200

    🚧 Add authentication and users model

---
+++ IsatiIntegration.API/Entities/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsatiIntegration.API.Entities
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Captain = "Captain";
+        public const string Player = "Player";
+    }
+}
---
+++ IsatiIntegration.API/Models/Users/RegisterModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsatiIntegration.API.Models.Users
+{
+    public class RegisterModel
+    {
+        /// <summary>
+        /// The user's profile picture
+        /// </summary>
+        public byte[] ProfilePicture { get; set; }
+
+        /// <summary>
+        /// The user's first name
+        /// </summary>
+        /// <example>Victor</example>
+        [Required]
+        public string FirstName { get; set; }
+        /// <summary>
+        /// The user's last name
+        /// </summary>
+        /// <example>DENIS</example>
+        [Required]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The user's email
+        /// </summary>
+        /// <example>admin@feldrise.com</example>
+        [Required]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The user's password
+        /// </summary>
+        /// <example>MySuperSecurePassword</example>
+        [Required]
+        public string Password { get; set; }
+    }
+}
