07d71c616f743dbe07f1a4bf67873073b0fe19ca
Feldrise <admin@feldrise.com>
2021-08-24 17:21:55 +0200

    🎉 Add files service

---
+++ IsatiIntegration.API/Services/Interfaces/IFilesService.cs
@@ -0,0 +1,16 @@
+﻿using IsatiIntegration.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsatiIntegration.API.Services.Interfaces
+{
+    public interface IFilesService
+    {
+        Task<string> UploadFile(string filename, byte[] fileBytes, bool shouldOverride = true);
+
+        Task<FileModel> GetFileById(string id);
+        Task<FileModel> GetFileByName(string name);
+    }
+}
