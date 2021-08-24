24196cb86185d1fda153e51dc0ce55c80fcd1596
Feldrise <admin@feldrise.com>
2021-08-24 17:44:51 +0200

    🚧 Add users service

---
+++ IsatiIntegration.API/Services/Interfaces/IUsersService.cs
@@ -0,0 +1,14 @@
+﻿using IsatiIntegration.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsatiIntegration.API.Services.Interfaces
+{
+    public interface IUsersService
+    {
+        Task<User> GetFullUser(string id);
+        Task<User> GetSensitiveUser(string id);
+    }
+}
---
+++ IsatiIntegration.API/Services/UsersService.cs
@@ -0,0 +1,43 @@
+﻿using IsatiIntegration.API.Entities;
+using IsatiIntegration.API.Services.Interfaces;
+using IsatiIntegration.API.Settings.Interfaces;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsatiIntegration.API.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IMongoCollection<User> _users;
+
+
+        public UsersService(IMongoSettings mongoSettings)
+        {
+            var mongoClient = new MongoClient(mongoSettings.ConnectionString);
+            var database = mongoClient.GetDatabase(mongoSettings.DatabaseName);
+
+            _users = database.GetCollection<User>(mongoSettings.UsersCollectionName);
+        }
+
+        public async Task<User> GetFullUser(string id)
+        {
+            var userCursor = await _users.FindAsync(dbUser =>
+                dbUser.Id == id
+            );
+
+            return await userCursor.FirstOrDefaultAsync();
+        }
+        public async Task<User> GetSensitiveUser(string id)
+        {
+            var fullUser = await GetFullUser(id);
+
+            // We need to remove sensitive field
+            fullUser.Email = null;
+
+            return fullUser;
+        }
+    }
+}
